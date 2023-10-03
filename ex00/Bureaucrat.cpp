8b4880b409acef48d78c73b130fff664a773b003
yena <nyj001012@gmail.com>
2023-10-03 14:45:37 +0900

    feat(ex00): implement ex00
    - class
    - main
    - Makefile

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@studen.42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 13:46:56 by yena              #+#    #+#             */
+/*   Updated: 2023/10/03 14:35:48 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//
+// Created by Yejin Na on 10/3/23.
+//
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void): _name("yena"), _grade(1) {
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) {
+  if (this != &other)
+    *this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other) {
+  if (this != &other) {
+    this->_grade = other._grade;
+  }
+  return (*this);
+}
+
+Bureaucrat::~Bureaucrat(void) {
+
+}
+
+std::string Bureaucrat::getName(void) const {
+  return (this->_name);
+}
+
+int Bureaucrat::getGrade(void) const {
+  return (this->_grade);
+}
+
+void Bureaucrat::increaseGrade(int grade) {
+  if (this->_grade - grade < 1)
+    throw (char *)INCREASE_GRADE_ERROR;
+  this->_grade -= grade;
+}
+
+void Bureaucrat::decreaseGrade(int grade) {
+  if (this->_grade + grade > 150)
+    throw (char *)DECREASE_GRADE_ERROR;
+  this->_grade += grade;
+}
