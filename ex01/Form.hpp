868befed289b8dcab3f6b43043db69360cc04b26
yena <50291995+nyj001012@users.noreply.github.com>
2023-10-04 14:31:12 +0900

    feat(ex01): add prototype of member function

--- ex01/Form.hpp
+++ ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: yena <yena@studen.42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 16:42:55 by yena              #+#    #+#             */
-/*   Updated: 2023/10/03 16:48:18 by yena             ###   ########.fr       */
+/*   Updated: 2023/10/04 14:28:11 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,42 @@
 #define EX01__FORM_HPP_
 
 #include <iostream>
+#include "Bureaucrat.hpp"
 
 /**
  * @private const std::string _name
- * @private bool _isSigned;
- * @private const int _grade
+ * @private bool _isSigned
+ * @private const int _grade (1 ~ 150)
  */
 class Form {
  private:
   const std::string _name;
   bool _isSigned;
   const int _grade;
+
+ public:
+  Form(std::string name, int grade);
+  Form(const Form &other);
+  Form &operator=(const Form &other);
+  ~Form();
+  const std::string getName() const;
+  bool getIsSigned() const;
+  int getGrade() const;
+  void beSigned(const Bureaucrat &bureaucrat);
+};
+
+class GradeTooHighException : public std::exception {
+ public:
+  virtual const char* what() const throw() {
+	return "Error: grade of Form is too high";
+  }
+};
+
+class GradeTooLowException : public std::exception {
+ public:
+  virtual const char* what() const throw() {
+	return "Error: grade of Form is too low"
+  }
 };
 
 #endif //EX01__FORM_HPP_
